5cee4c958c1128a2d4e0a02841882e492779d528
Vijay Babu <vb9477998@gmail.com>
2026-02-28 09:22:01 +0530

    Add variables example

---
+++ 01_basics/variables.py
@@ -0,0 +1,11 @@
+# Variables and Data Types
+
+name = "Vijay"
+age = 20
+height = 5.8
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
