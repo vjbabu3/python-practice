bd1432575aa4c5c28819c35c1ec26e7cb05696b9
Vijay Babu <vb9477998@gmail.com>
2026-03-02 15:11:19 +0530

    Add functions examples

---
+++ 01_basics/functions.py
@@ -0,0 +1,15 @@
+# Functions in Python
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def add(a, b):
+    return a + b
+
+def square(number):
+    return number * number
+
+
+print(greet("Vijay"))
+print("Sum:", add(10, 5))
+print("Square:", square(4))
