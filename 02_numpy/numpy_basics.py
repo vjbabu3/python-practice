de2cc2df56c356a70912e5d6ef2cbd29517801ae
Vijay Babu <vb9477998@gmail.com>
2026-03-04 19:19:55 +0530

    Add NumPy basics example

---
+++ 02_numpy/numpy_basics.py
@@ -0,0 +1,23 @@
+# NumPy Basics
+
+import numpy as np
+
+# Creating arrays
+arr = np.array([1, 2, 3, 4, 5])
+print("Array:", arr)
+
+# Array operations
+print("Array + 5:", arr + 5)
+print("Array * 2:", arr * 2)
+
+# Creating zeros and ones
+zeros = np.zeros(3)
+ones = np.ones(3)
+
+print("Zeros:", zeros)
+print("Ones:", ones)
+
+# 2D Array
+matrix = np.array([[1, 2], [3, 4]])
+print("Matrix:")
+print(matrix)
