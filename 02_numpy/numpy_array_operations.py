bbf9649dd93faf22cf83016b66203f3920713c4c
Vijay Babu <vb9477998@gmail.com>
2026-03-06 19:26:40 +0530

    Add numpy array operations example

---
+++ 02_numpy/numpy_array_operations.py
@@ -0,0 +1,14 @@
+# NumPy Array Operations
+
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+print("Array 1:", arr1)
+print("Array 2:", arr2)
+
+print("Addition:", arr1 + arr2)
+print("Subtraction:", arr1 - arr2)
+print("Multiplication:", arr1 * arr2)
+print("Division:", arr1 / arr2)
