c428c43fa36f5f12de881955ebc4026896652356
Vijay Babu <vb9477998@gmail.com>
2026-03-08 08:38:06 +0530

    Add numpy matrix operations example

---
+++ 02_numpy/numpy_matrix_operations.py
@@ -0,0 +1,18 @@
+# NumPy Matrix Operations
+
+import numpy as np
+
+matrix1 = np.array([[1, 2], [3, 4]])
+matrix2 = np.array([[5, 6], [7, 8]])
+
+print("Matrix 1:")
+print(matrix1)
+
+print("Matrix 2:")
+print(matrix2)
+
+print("Matrix Addition:")
+print(matrix1 + matrix2)
+
+print("Matrix Multiplication:")
+print(np.dot(matrix1, matrix2))
